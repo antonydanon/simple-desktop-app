0aeac164013bb5dc2e8f90066225519930ae3e3f
Anton <antondanik32@mail.ru>
2022-07-15 18:10:33 +0300

    Create class InstanceCounter (first_task)

---
+++ src/main/java/com/desktop/simple_app/first_task/InstanceCounter.java
@@ -0,0 +1,22 @@
+package com.desktop.simple_app.first_task;
+
+public class InstanceCounter {
+    private static int instanceCount;
+
+    InstanceCounter(){
+        instanceCount++;
+    }
+
+    public static int getInstanceCount(){
+        return instanceCount;
+    }
+
+    public static void main(String[] args) {
+        InstanceCounter i1 = new InstanceCounter();
+        InstanceCounter i2 = new InstanceCounter();
+        System.out.println(getInstanceCount());
+
+        InstanceCounter i3 = new InstanceCounter();
+        System.out.println(getInstanceCount());
+    }
+}
