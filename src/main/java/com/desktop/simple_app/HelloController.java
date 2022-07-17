d3d60ce119645a843d0305721fba4313eafce8ed
Anton <antondanik32@mail.ru>
2022-07-17 19:24:40 +0300

    Create third_task

--- src/main/java/com/desktop/simple_app/HelloController.java
+++ src/main/java/com/desktop/simple_app/HelloController.java
@@ -1,14 +1,62 @@
 package com.desktop.simple_app;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    private ResourceBundle resourceBundle;
+
+    private boolean isEnglish = true;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label helloWorldLabel;
+
+    @FXML
+    private Button sayHelloButton;
+
+    @FXML
+    private Label languageLabel;
+
+    @FXML
+    private Button changeLanguageButton;
+
+    @FXML
+    void initialize() {
+        loadLanguage("en");
+    }
+
+    @FXML
+    private void changeLanguage() {
+        if(isEnglish){
+            isEnglish = false;
+            loadLanguage("fra");
+        } else {
+            isEnglish = true;
+            loadLanguage("en");
+        }
+
+    }
+
+    @FXML
+    private void sayHelloWorld() {
+        helloWorldLabel.setText(resourceBundle.getString("helloWorldLabel"));
+    }
+
+    private void loadLanguage(String lang) {
+        Locale locale = new Locale(lang);
+        resourceBundle = ResourceBundle.getBundle("com/desktop/simple_app/language", locale);
+        helloWorldLabel.setText("");
+        initializeElements();
+    }
+
+    private void initializeElements() {
+        sayHelloButton.setText(resourceBundle.getString("sayHelloButton"));
+        changeLanguageButton.setText(resourceBundle.getString("changeLanguageButton"));
+        languageLabel.setText(resourceBundle.getString("languageLabel"));
     }
 }
